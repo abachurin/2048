e633d1bd936b5a315d915a81122a4ed491e5415d
Alexey Bachurin <bachurin_a@nlmktrading.com>
2022-07-06 07:59:22 +0100

    got rid of log-reward and scaled decay fixed mem lry leak

--- application.py
+++ application.py
@@ -882,3 +882,4 @@ if __name__ == '__main__':
     # make_empty_status(); sys.exit()
     # app.run_server(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=(LOCAL == 'local'), use_reloader=False)
     application.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=(LOCAL == 'local'), use_reloader=False)
+
